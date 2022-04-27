0774215505026aef183f0abdaa17ec55918765d8
GiftXXVI <chimphonda.gift@gmail.com>
2022-04-27 10:24:58 +0200

    Removed scripts

--- env.sh
+++
@@ -1,8 +0,0 @@
-export 'DB_NAME'=Fyyur
-export 'DB_USER'=postgres
-export 'DB_PASSWD'=5n22X6nTbgvSJHbA
-export 'DB_HOST'=localhost
-export 'DB_PORT'=5432
-export 'FLASK_APP'=app
-export 'FLASK_DEBUG'=True
-export 'FLASK_ENV'=development
